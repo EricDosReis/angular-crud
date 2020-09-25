d8527ad18147155e669c6a7b25cb285482c2d910
EricDosReis <ericdosreis.personal@gmail.com>
2020-09-25 17:18:11 -0300

    feat: create app routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { NavComponent } from './components/layout/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+import { OrdersComponent } from './views/orders/orders.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductsComponent } from './views/products/products.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    HomeComponent,
+    OrdersComponent,
+    ProductCreateComponent,
+    ProductsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
